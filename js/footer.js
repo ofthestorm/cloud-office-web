31044a5a09ad87c59222ecd3bc075358c476849f
ofthestorm <870771017@qq.com>
2017-08-29 23:18:37 +0800

    ~

---
+++ js/footer.js
@@ -0,0 +1,3 @@
+document.writeln("<div class=\'footer\'>");
+document.writeln("  <p class=\'center-block\'>Copyright &copy; 2017 Tongji SSE</p>");
+document.writeln("</div>");
--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
-$('.form_datetime').datetimepicker({
-    language:  'zh-CN',
-    weekStart: 1,
-    todayBtn:  1,
-    autoclose: 1,
-    todayHighlight: 1,
-    startView: 2,
-    forceParse: 0,
-    showMeridian: 1
-});
+
 
 $(function () {
+    //window height
     var window_height = $(window).height();
-    $(".pre-scrollable").css("max-height", window_height);
+    $('.pre-scrollable').css("max-height", window_height);
+
+    //sidebar click event
+    //拿到上一层，对每个子元素判断
+    $('#side-attendance').bind("click",function (event) {
+        $('#attendance').removeClass("hide");
+    })
+    $('#side-leave').bind("click",function (event) {
+        $('#leave').removeClass("hide");
+    })
+
+    function hideOrShow(id) {
+        if($(id).hasClass("hide")) {
+
+        }
+    }
+
+    //datetime picker
+    $('.form_datetime').datetimepicker({
+        language:  'zh-CN',
+        weekStart: 1,
+        todayBtn:  1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        forceParse: 0,
+        showMeridian: 1
+    });
 })
+
+
