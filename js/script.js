98de31d3e756c0306f5dee59def61d0e5c63913e
ofthestorm <870771017@qq.com>
2017-08-30 22:09:27 +0800

    leave

--- js/script.js
+++ js/script.js
@@ -1,35 +1,47 @@
-
-
 $(function () {
-    //window height
+    //scrollbar适应当前高度
     var window_height = $(window).height();
     $('.pre-scrollable').css("max-height", window_height);
 
-    //sidebar click event
-    //拿到上一层，对每个子元素判断
-    $('#side-attendance').bind("click",function (event) {
-        $('#attendance').removeClass("hide");
-    })
-    $('#side-leave').bind("click",function (event) {
-        $('#leave').removeClass("hide");
-    })
+    //sidebar click
+    $('.nav-sidebar li').click(function () {
+        console.log('第'+$(this).index()+'个li被点击');
+        $(this).addClass("sidebar-active").siblings('li').removeClass("sidebar-active");
 
-    function hideOrShow(id) {
-        if($(id).hasClass("hide")) {
-
+        switch ($(this).index()) {
+            case 0: {
+                $('#attendance').removeClass("hide").siblings('.module').addClass("hide");
+            }break;
+            case 1: {
+                $('#leave').removeClass("hide").siblings('.module').addClass("hide");
+            }break;
+            case 2: {
+                $('#overtime').removeClass("hide").siblings('.module').addClass("hide");
+            }break;
+            default: break;
         }
-    }
+    });
+    $('.nav-sidebar').children('li').each(function (index, element) {
+        // if(element.)
+    })
+    // $('#side-attendance').bind("click",function (event) {
+    //     $('#attendance').removeClass("hide");
+    // })
+    // $('#side-attendance').
+    // $('#side-leave').bind("click",function (event) {
+    //     $('#leave').removeClass("hide");
+    // })
 
     //datetime picker
-    $('.form_datetime').datetimepicker({
+    $('.form_date').datetimepicker({
         language:  'zh-CN',
         weekStart: 1,
         todayBtn:  1,
         autoclose: 1,
         todayHighlight: 1,
         startView: 2,
-        forceParse: 0,
-        showMeridian: 1
+        minView: 2,
+        forceParse: 0
     });
 })
 
